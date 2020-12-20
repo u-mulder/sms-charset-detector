729ac566dbe5f10069198c3eccfcffad492c57b8
u_mulder <m264695502@gmail.com>
2020-12-20 20:45:07 +0300

    :white_check_mark: Add draft tests for BasicCharsetDetector

--- test/BasicCharsetDetectorTest.php
+++ test/BasicCharsetDetectorTest.php
@@ -2,20 +2,74 @@
 
 declare(strict_types=1);
 
-class BasicCharsetDetectorTest extends
+class BasicCharsetDetectorTest extends 
 {
     /**
-     * @dataProvider
+     * @dataProvider provideMessages
      */
-    public function test(string $message, bool $isGsmCharset): voids
+    public function testDetectCharset(string $message, bool $includeExtensionSet, string $expectedCharset): void
     {
+		$detector = $this->getCharsetDetector($includeExtensionSet);
+		
+		$this->assertSame(
+			$isGsmCharset,
+			$detector->detectCharset($message)
+		);
     }
 
+    /**
+     * @dataProvider 
+     */
+    public function testIsGsmCharset(string $message, bool $includeExtensionSet, bool $isGsmCharset): void
+    {
+		$detector = $this->getCharsetDetector($includeExtensionSet);
+
+		$this->assertSame(
+			$isGsmCharset,
+			$detector->isGsmCharset($message)
+		);
+    }
+    
+    
+    public function testGetGsmCharsetAsJson(): void
+    {
+        $detector = $this->getCharsetDetector(true);
+
+		$this->assertString($detector->getGsmCharsetAsJson());
+
+        $detector->setIncludeExtensionSet(false);
+        
+		$this->assertString($detector->getGsmCharsetAsJson());
+    }
+	
+    // TODO
+	public function provideMessages(): \Generator	// array
+	{
+		$messages = [];
+		return [
+			[],
+			[],
+			[],
+			[],
+			[],
+			[],
+			[],
+			[],
+			[],
+		];
+	}
+    
     public function provideMessages(): array
     {
         return [
 
 
         ];
+    }
+
+    
+    protected function getCharsetDetector(bool $includeExtensionSet): BasicCharsetDetector
+    {
+        return new Um\BasicCharsetDetector($includeExtensionSet);
     }
 }

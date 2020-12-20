b99d1acee2619fd140fddfaae3b9571cd4efc5cf
u_mulder <m264695502@gmail.com>
2020-12-20 13:26:11 +0300

    :art: Improve code structure: tabs to spaces and error fixing

--- src/BasicCharsetDetector.php
+++ src/BasicCharsetDetector.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-namespace Um/CharsetDetector;
+namespace Um\CharsetDetector;
 
 class BasicCharsetDetector implements CharsetDetectorInterface
 {
-	/**
+    /**
      * Basic gsm characters
      *
      * @see https://en.wikipedia.org/wiki/GSM_03.38
      */
-	protected const GSM_BASIC_SET = [
+    protected const GSM_BASIC_SET = [
         '@', 'Δ', ' ', '0', '¡', 'P', '¿', 'p',
         '£', '_', '!', '1', 'A', 'Q', 'a', 'q',
         '$', 'Φ', '"', '2', 'B', 'R', 'b', 'r',
@@ -28,70 +28,59 @@ class BasicCharsetDetector implements CharsetDetectorInterface
         "\r", 'æ', '-', '=', 'M', 'Ñ', 'm', 'ñ',
         'Å', 'ß', '.', '>', 'N', 'Ü', 'n', 'ü',
         'å', 'É', '/', '?', 'O', '§', 'o', 'à',
-	];
+    ];
 
-    /**
-     * Extension set characters, each occupy two positions in sms
-     */
-	protected const GSM_EXTENSION_SET = [
-		'|', '^', '€', '{', '}', '[', '~', ']', '\\',
-	];
-	
-	/**
-     * Use extension set characters or not when detecting the charset
-     */
-	protected $includeExtensionSet = true;
+    /** Extension set characters, each occupies two positions in sms */
+    protected const GSM_EXTENSION_SET = [
+        '|', '^', '€', '{', '}', '[', '~', ']', '\\',
+    ];
 
-	/**
-     * Current charset, stores either all characters or ones from basic set only
-     */
-	protected $currentCharset = [];
-	
-	public function __construct(bool $includeExtensionSet) 
-	{
-		$this->setIncludeExtensionSet($includeExtensionSet);
-	}
-	
-	public function setIncludeExtensionSet(bool $includeExtensionSet): self 
-	{
-		$this->includeExtensionSet = $includeExtensionSet;
+    /** Use extension set characters or not when detecting the charset */
+    protected $includeExtensionSet = true;
 
-		$this->currentCharset = $this->includeExtensionSet
-			? static::GSM_BASIC_SET
-			: array_merge(static::GSM_BASIC_SET, static::GSM_EXTENSION_SET);
+    /** Current charset, stores either all characters or ones from basic set only */
+    protected $currentCharset = [];
 
-		return $this;
-	}
-	
-	public function getIncludeExtensionSet(): bool
-	{
-		return $this->includeExtensionSet;
-	}
-	
-	public function detectCharset(string $message): string
-	{
-        if ('' === $string) {
+    public function __construct(bool $includeExtensionSet)
+    {
+        $this->setIncludeExtensionSet($includeExtensionSet);
+    }
+
+    public function setIncludeExtensionSet(bool $includeExtensionSet): self
+    {
+        $this->includeExtensionSet = $includeExtensionSet;
+
+        $this->currentCharset = $this->includeExtensionSet
+            ? static::GSM_BASIC_SET
+            : array_merge(static::GSM_BASIC_SET, static::GSM_EXTENSION_SET);
+
+        return $this;
+    }
+
+    public function getIncludeExtensionSet(): bool
+    {
+        return $this->includeExtensionSet;
+    }
+
+    public function detectCharset(string $message): string
+    {
+        if ('' === $message) {
             return static::GSM_CHARSET;
         }
 
-        $charsCount = \mb_strlen($string, 'UTF-8');
+        $charsCount = \mb_strlen($message, 'UTF-8');
         for ($i = 0; $i < $charsCount; ++$i) {
-            $currentChar = \mb_substr($string, $i, 1);
+            $currentChar = \mb_substr($message, $i, 1);
             if (!in_array($currentChar, $this->currentCharset, true)) {
                 return static::UCS_CHARSET;
             }
         }
 
         return static::GSM_CHARSET;
-	}
+    }
 
-	public function isGsmCharset(string $message): bool 
-	{
-		return static::GSM_CHARSET === static::detectCharset($message);
-	}
-
-	public function getGsmCharsetAsJson(): string 
-	{
-		return json_encode($this->currentCharset);
-	}
+    public function isGsmCharset(string $message): bool
+    {
+        return static::GSM_CHARSET === $this->detectCharset($message);
+    }
 }
--- test/BasicCharsetDetectorTest.php
+++ test/BasicCharsetDetectorTest.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 
 class BasicCharsetDetectorTest extends
 {
-	/**
-	 * @dataProvider 
-	 */
-	public function test(string $message, bool $isGsmCharset): voids
-	{
-		
-	}
-	
-	public function provideMessages(): array
-	{
-		return [
-		
-		
-		
-		
-		];
-	}
+    /**
+     * @dataProvider
+     */
+    public function test(string $message, bool $isGsmCharset): voids
+    {
+    }
+
+    public function provideMessages(): array
+    {
+        return [
+
+
+        ];
+    }
 }
